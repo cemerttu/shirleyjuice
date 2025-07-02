caf6ec22a3e93191987f41c919444ec2e2e173da
ian <cemenrttu@gmail.com>
2025-07-02 12:16:14 +0300

    firstcommit

---
+++ src/componest/componentOne.js
@@ -0,0 +1,19 @@
+function Navbar(){
+    return(
+        <nav>
+            <ul>
+                <li>
+                    <a href="#home">Home</a>
+                </li>
+                <li>
+                    <a href="#about">About</a>
+                </li>
+                <li>
+                    <a href="#contant">Contact</a>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
