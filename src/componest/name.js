cc687e3f854a33e56047b6b3c5d27dec0f0d2f96
ian <cemenrttu@gmail.com>
2025-07-08 15:12:49 +0300

    commit

--- src/componest/name.js
+++ src/componest/name.js
@@ -1,32 +1,9 @@
 import {userState} from 'react';
 
-
-const  NameForm = ({onSubmitN}) =>{
-    const [name, setName] = userState('');
-    
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        onSubmitN(name);
-        setName('');
-    };
-    
-    return (
-        <form onSubmit={handleSubmit}>
-        <label>
-            Name:
-            <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            />
-        </label>
-        <button type="submit">Submit</button>
-        </form>
-    );
+const NameForm = () => {
+  const [name, setName] = userState('');
 }
-
 export default NameForm;
-
 
 
 
