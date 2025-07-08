fcf0ca1be8538a12ac56cffb6484eaf5402fadc2
ian <cemenrttu@gmail.com>
2025-07-08 15:18:38 +0300

    commit

--- src/componest/profilecomponent.js
+++ src/componest/profilecomponent.js
@@ -1,13 +1,20 @@
 function profileComponent() {
   return (
-   <div className="profile-component">
-    <div className="profile-message">
-      <h2>Insert your name?</h2>
-    </div>
-   </div>
+    <>
+      <nav>Navbar</nav>
+      <div className="profile-component">
+        <div className="profile-message">
+          <h2>Insert your name?</h2>
+        </div>
+      </div>
+    </>
   );
 }
 
 export default profileComponent;
+
+
+
+
 
 
