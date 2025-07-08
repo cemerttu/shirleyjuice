f30895050ddbe7dbc669eee5eeb36f72b2a2af02
ian <cemenrttu@gmail.com>
2025-07-08 15:56:15 +0300

    commit

--- src/componest/name.js
+++ src/componest/name.js
@@ -2,11 +2,29 @@ import {userState} from 'react';
 
 const NameForm = () => {
   const [name, setName] = userState('');
+  const  habndleSubmit = (e) => {
+    e.preventDefault();
+    if (name.trim()) {
+      onSubmitName(name);
+      setName('');
+    }
+  };
 
-
-  
+  return (
+    <div className='name-form'>
+      <form onSubmit={handleSubmit}>
+        <input
+        type = "text"
+        id = "name"
+        value = {name}
+        onChange={(e) => setName(e.target.value)} 
+        placeholder='Enter your name'
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 export default NameForm;
-
 
 
--- src/componest/profilecomponent.js
+++ src/componest/profilecomponent.js
@@ -1,13 +1,24 @@
+import {userState} from 'react';
+import NameForm from '/name';
 function profileComponent() {
+  const [username, setUsername] = userState('');
+  
   return (
-    <>
-      <nav>Navbar</nav>
       <div className="profile-component">
-        <div className="profile-message">
-          <h2>Insert your name?</h2>
+        {username ? (
+          <div className="profile-message">
+            <h2>Welcome back, {username}</h2>
+            <p>This is your </p>
+          </div>
+        ) : (
+          <NameForm onSubmitName={setUsername} />
+        )}
+        <div className='Profile-image'>
+          <img src='https://via.placeholder.com/150' alt='Profile' />
         </div>
       </div>
-    </>
+
+
   );
 }
 
